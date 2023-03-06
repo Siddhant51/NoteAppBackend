0e8e503ec4cfc96fbdbeba73605444ffd94b5759
Siddhant51 <124143317+Siddhant51@users.noreply.github.com>
2023-03-07 02:01:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(require("./routes"));
 
 const middleware = (req, res, next) => {
